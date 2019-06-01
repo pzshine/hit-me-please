83b0985e0dac9034e36b40593babac25f95b9b0c
Shine <s.sutheeravet@gmail.com>
2019-06-01 11:23:20 +0700

    Update Test file

--- hit_me_please/hitters/tests.py
+++ hit_me_please/hitters/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Hitter
+class HittersTest(TestCase):
+  def test_hitter_modal_shold_have_email_field(self):
+    hitter = Hitter.objects.create(
+      email='shine@prontomarketing.com'
+    )
+    self.assertEqual(hitter.email, 'shine@prontomarketing.com')
 
 # Create your tests here.
