e42602efbd99ddedcb7a0235415afacaa80f92cd
Shine <s.sutheeravet@gmail.com>
2019-06-01 15:04:24 +0700

    :sparkles: Add new email test case :sunny:

--- hit_me_please/hitters/views.py
+++ hit_me_please/hitters/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+
+from .models import Hitter
+
 class LandingPageView(View):
   def get(self, request):
     return render(
       request,
       'index.html'
     )
+  def post(self, request):
+    email = request.POST.get('email')
+    Hitter.objects.create(email=email)
+    return HttpResponse()
