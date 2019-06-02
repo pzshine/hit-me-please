abf19d8e2bbd5e4766b32da22991d09797027b24
Shine <s.sutheeravet@gmail.com>
2019-06-02 10:23:33 +0700

    :wrench: Create one simple compute instance :sunny:

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,5 +2,32 @@ provider "google" {
   credentials = "${file("hitmebaby.json")}"
   project     = "just-advice-242401"
   region      = "asia-southeast1"
-  zone        = "asia-southeast1-b"
+  zone        = "asia-southeast1-c"
+}
+
+resource "google_compute_instance" "tf_instance" {
+  name         = "tf-instance-${count.index}"
+  machine_type = "n1-standard-1"
+  count        = 1
+  boot_disk {
+    initialize_params {
+	    image = "ubuntu-1804-bionic-v20190530"
+			size  = 30
+		}
+	}
+
+	metadata = {
+		sshKeys = "s.sutheeravet:${file("~/.ssh/id_rsa.pub")}"
+	}
+
+	network_interface {
+		network       = "default"
+		access_config {}
+	}
+
+	tags = ["my-web"]
+}
+
+output "ip" {
+	value = "${google_compute_instance.tf_instance.*.network_interface.0.access_config.0.nat_ip}"
 }
